af56e19a38342785b4a76e34f39adc8df9589250
DanielSam23 <115190742+DanielSam23@users.noreply.github.com>
2024-11-14 18:10:48 -0300

    Add files via upload

---
+++ Controle-Clientes/PessoaFisica.cs
@@ -0,0 +1,12 @@
+namespace Controle_Clientes {
+
+    public class PessoaFisica: Clientes {
+
+        public string cpf {get;set;}
+
+        public string rg {get;set;}
+
+        
+
+    }
+}
---
+++ Controle-Clientes/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using Controle_Clientes;
+
+Console.WriteLine ("informar nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine ("informar endereco:");
+string endereco = Console.ReadLine();
+
+Console.WriteLine ("Pessoa Fisica (f) ou Pessoa Juridica (j)?" );
+string tipo = Console.ReadLine();
+
+if(tipo == "f") {
+    
+    PessoaFisica pf = new PessoaFisica() {
+        nome = nome,
+        endereco = endereco,
+    };
+    Console.WriteLine("informar o cpf:");
+    pf.cpf = Console.ReadLine();
+
+    Console.WriteLine("informar o rg:");
+    pf.rg = Console.ReadLine();
+
+    Console.WriteLine("informar o valor de compra:");
+    float valor = float.Parse(Console.ReadLine());
+
+    pf.Pagar_Imposto(valor);
+
+    Console.WriteLine("----------PessoaFisica Fisica ----------");
+    Console.WriteLine("Nome: " + pf.nome);
+    Console.WriteLine("Endereco: " + pf.endereco);
+    Console.WriteLine("CPF: " + pf.cpf);
+    Console.WriteLine("RG: " + pf.rg);
+    Console.WriteLine("Valor de compra: " + pf.valor.ToString("C"));
+    Console.WriteLine("Imposto: " + pf.valorImposto);
+    Console.WriteLine("Total a pagar:" + pf.total.ToString("C"));
+    
+    
+}
+
+else if(tipo == "j"){
+
+    PessoaJuridica pj = new PessoaJuridica() {
+            nome = nome,
+            endereco = endereco,
+        };
+
+    Console.WriteLine("Informar CNPJ: " + pj.cnpj);
+    pj.cnpj = Console.ReadLine();
+
+    Console.WriteLine("Informar IE: ");
+    pj.ie = Console.ReadLine();
+
+    Console.WriteLine("informar o valor de compra:");
+    float valor = float.Parse(Console.ReadLine());
+
+    pj.Pagar_Imposto(valor);
+
+     Console.WriteLine("----------PessoaFisica Juridica ----------");
+    Console.WriteLine("Nome: " + pj.nome);
+    Console.WriteLine("Endereco: " + pj.endereco);
+    Console.WriteLine("CNPJ: " + pj.cnpj);
+    Console.WriteLine("IE: " + pj.ie);
+    Console.WriteLine("Valor de compra: " + pj.valor.ToString("C"));
+    Console.WriteLine("Imposto: " + pj.valorImposto);
+    Console.WriteLine("Total a pagar:" + pj.total.ToString("C"));
+
+
+
+
+}
